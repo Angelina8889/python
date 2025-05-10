e07cb845aa2ff4f8827f80c8e1bd8da15f4054bf
Angelina8889 <faranova00@mail.ru>
2025-05-10 12:50:29 +0300

    Reshenie zadachi SLOVAR.py

---
+++ if-else.py
@@ -0,0 +1,36 @@
+a = 5
+b = 5
+if a == b:
+    print("a равно b")
+else:
+    print("a НЕ равно b")
+
+print("__________________")
+
+cost=2000
+if cost < 1000:
+   print("скидки нет")
+elif cost < 3000:
+    print("скидка 5%")
+elif cost < 5000:
+    print("скидка 7%")
+else:
+    print("скидка 10%")
+
+print("-------")
+a = int(input("Введите целое число: "))
+if a>5:
+    print("Это число больше пяти")
+
+print("********")
+a = int(input("Введите целое число: "))
+if a>0:
+   a += 1
+else:
+    a -= 2
+print(a)
+
+# print("^^^^^^^^")
+# input("Введите число: ")
+# if
+
