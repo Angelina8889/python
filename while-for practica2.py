a524856fc05ba7466ac39450bcc88b163b3ede4d
Angelina8889 <faranova00@mail.ru>
2025-05-18 12:56:47 +0300

    while-for.py

---
+++ while-for practica2.py
@@ -0,0 +1,50 @@
+#задача 1
+n = 0
+while n < 50:
+    n +=2
+    print(n)
+
+#задача 2
+sum = 0
+y = 1
+while y <=100:
+    sum += y
+    y += 1
+print("Сумма чисел от 1 до 100:", sum)
+
+#задача 3
+n= int(input("Введите число: "))
+is_prime = True
+
+if n < 2:
+    is_prime = False
+else:
+    for i in range (2, n):
+        if n % i == 0
+            is_prime = false
+            break
+
+if is_prime = True
+    print("Число простое")
+else:
+    print("Числе не является простым")
+
+#задача 4
+n= int(input("Введите число: "))
+result = 1
+for i in range(2, n+1):
+    result *= i
+    print(result)
+
+
+
+
+
+
+
+#задача 6
+for числа in range (10, 0 , -1):
+    print(числа)
+
+
+
--- while-for.py
+++ while-for.py
@@ -9,28 +9,36 @@
 
 
 #Задача 8
-sales1 = float(input("Введите уровень продаж для менеджера 1: "))
-sales2 = float(input("Введите уровень продаж для менеджера 2: "))
-sales3 = float(input("Введите уровень продаж для менеджера 3: "))
+# sales1 = float(input("Введите уровень продаж для менеджера 1: "))
+# sales2 = float(input("Введите уровень продаж для менеджера 2: "))
+# sales3 = float(input("Введите уровень продаж для менеджера 3: "))
+#
+# salaries=[]
+# for sales in [sales1, sales2, sales3]:
+#     if sales <= 500:
+#       percent = 0.03
+#     elif sales1 <= 1000:
+#         percent = 0.05
+#     else:
+#         percent = 0.08
+#     salary = 200 + sales * percent
+#     salaries.append(salary)
+#
+#
+# best_salary = max(salaries)
+# best_index  = salaries.index(best_salary)
+# salaries[best_index] += 200
+#
+# print(f"Зарплата 1 менеджера: {salaries[0]}")
+# print(f"Зарплата 1 менеджера: {salaries[1]}")
+# print(f"Зарплата 1 менеджера: {salaries[2]}")
+# print(f"Лучший менеджер: {best_index + 1}-ый")
 
-salaries=[]
-for sales in [sales1, sales2, sales3]:
-    if sales <= 500:
-      percent = 0.03
-    elif sales1 <= 1000:
-        percent = 0.05
-    else:
-        percent = 0.08
-    salary = 200 + sales * percent
-    salaries.append(salary)
+# for x in range(5, 50, 5):
+#     print(x)
+#
+# for x in range(50, 5, -5):
+#     print(x)
 
-
-best_salary = max(salaries)
-best_index  = salaries.index(best_salary)
-salaries[best_index] += 200
-
-print(f"Зарплата 1 менеджера: {salaries[0]}")
-print(f"Зарплата 1 менеджера: {salaries[1]}")
-print(f"Зарплата 1 менеджера: {salaries[2]}")
-print(f"Лучший менеджер: {best_index + 1}-ый")
-
+for x,y in enumerate(["Первый","Второй", "Третий"]):
+    print(x, "-", y)
